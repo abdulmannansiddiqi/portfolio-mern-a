b19f90fb21418bd117406e87a6c76724efd12cf5
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2026-08-15 22:04:42 +0500

    Connect Express server to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
