26801297e93028ca0149df3172e6262d24d249a1
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2026-08-17 05:32:54 +0500

    ...

--- server/index.js
+++ server/index.js
@@ -8,8 +8,7 @@ const projectsRoute = require('./routes/projects');
 const chatRoute = require('./routes/chat');
 
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
